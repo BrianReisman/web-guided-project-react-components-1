eeec7dbd5af278f49d5efec7c498145f36cb66a7
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-10-05 18:29:07 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ import { render } from 'react-dom'
 */
 
 // we have MANY of these
-function Foo(props) { // one object called "props"
+function foo(props) { // one object called "props"
   return (
     <div className='container'>
       <h1>Welcome to React, Web {props.cohort}</h1>
@@ -33,6 +33,6 @@ function Foo(props) { // one object called "props"
 
 // only once per app, usually
 render(
-  <App cohort='36' instructor='Gabe' />, // element(s)
+  <foo cohort='36' instructor='Gabe' />, // element(s)
   document.querySelector('#root')
 )
